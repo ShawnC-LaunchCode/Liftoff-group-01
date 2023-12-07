6bbb13c5259220921bbd3c2ef737729eb4fad0f6
Brittany Robinson <bncopple@gmail.com>
2023-12-06 20:10:50 -0600

    react app created

--- dressme-frontend/src/App.js
+++ dressme-frontend/src/App.js
@@ -3,18 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Welcome to Dress Me!!</h1>
-
-    <h3>Please Log In to Your Account</h3>
     <div class="loginForm">
-    <form action="user-homepage.html" method="post">
-       <label>First Name <input type="text" name="firstName"/> </label>
-        <label>Last Name <input type="text" name="lastName"/> </label>
-        <label>Email<input type="email" name="emailAddress"/></label>
-        <label>Your 5-digit Zip Code <input type="number" name="zipCode" /></label>
-        <button id="loginButton" type="submit">Log In</button>
-    </form>
+<h1 class="welcomePageHeader">Welcome to Dress Me!!</h1>
+<h3>Please Log In to Your Account</h3>
+<div>
+<form action="user-homepage.html" method="post">
+   <label>First Name <input type="text" name="firstName"/> </label>
+    <label>Last Name <input type="text" name="lastName"/> </label>
+    <label>Email<input type="email" name="emailAddress"/></label>
+    <label>Your 5-digit Zip Code <input type="number" name="zipCode" /></label>
+    <button id="loginButton" type="submit">Log In</button>
+</form>
+</div>
+
     </div>
+    
   );
 }
 
