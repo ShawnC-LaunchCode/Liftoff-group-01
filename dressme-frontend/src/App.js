dd49b2cbd0d24d79b4fbdc2fe8fe44d964d7dd79
Brittany Robinson <bncopple@gmail.com>
2023-12-06 19:27:18 -0600

    code moved to front end

--- dressme-frontend/src/App.js
+++ dressme-frontend/src/App.js
@@ -3,10 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       Welcome to Dress Me!
-      </header>
+    <h1>Welcome to Dress Me!!</h1>
+
+    <h3>Please Log In to Your Account</h3>
+    <div class="loginForm">
+    <form action="user-homepage.html" method="post">
+       <label>First Name <input type="text" name="firstName"/> </label>
+        <label>Last Name <input type="text" name="lastName"/> </label>
+        <label>Email<input type="email" name="emailAddress"/></label>
+        <label>Your 5-digit Zip Code <input type="number" name="zipCode" /></label>
+        <button id="loginButton" type="submit">Log In</button>
+    </form>
     </div>
   );
 }
