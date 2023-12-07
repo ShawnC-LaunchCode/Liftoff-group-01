02047870a52b533a30ffc501fd36e4f1a3ad4bb7
Brittany Robinson <bncopple@gmail.com>
2023-12-07 17:56:28 -0600

    set dress code form on homepage

--- weather2wear/src/App.js
+++ weather2wear/src/App.js
@@ -4,20 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 class="welcomePageHeader">Welcome to Dress Me!!</h1>
+<h3>Please Log In to Your Account</h3>
+<div>
+<form action="user-homepage.html" method="post">
+   <label>First Name <input type="text" name="firstName"/> </label>
+    <label>Last Name <input type="text" name="lastName"/> </label>
+    <label>Email<input type="email" name="emailAddress"/></label>
+    <label>Your 5-digit Zip Code <input type="number" name="zipCode" /></label>
+    <button id="loginButton" type="submit">Log In</button>
+</form>
+</div>
     </div>
   );
 }
