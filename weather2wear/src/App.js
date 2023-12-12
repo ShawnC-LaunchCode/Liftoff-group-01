e3ec7202524529e84887979d30a3422ea0c81a4c
Brittany Robinson <bncopple@gmail.com>
2023-12-11 20:01:22 -0600

    routes created and working between pages

--- weather2wear/src/App.js
+++ weather2wear/src/App.js
@@ -1,28 +1,37 @@
 import { Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
 import './Pages/userHomepage.js'
 import Header from './Components/Header.js';
 import LoginForm from './Pages/LoginForm.js';
 import CreateAccount from './Pages/CreateAccount.js';
+import userHomepage from './Pages/userHomepage.js';
 import { Component } from 'react';
+import './Pages/userHomepage.js'
+import Home from './Pages/Home.js';
 
 
 function App() {
   
   return (
     <div className="App">
-      <Header/>
-      <LoginForm/>
-      <CreateAccount/>
-<div buttons>
+    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/CreateAccount" element={<CreateAccount />} />
+        <Route path="/LoginForm" element={<LoginForm />} />
+        <Route path="/HomePage" element={<userHomepage/>} />
+      </Routes>
+    
+{/* <div buttons>
 
 <button name="logInButton" id="logInButton">Log in to Your Account</button>
 
 <br/> <br/> 
-<a href="./Components/CreateAccount.js"/>
 <button name="createAccountButton">Create Account</button>
 
-</div>
+</div> */}
     </div>
   );
 }
