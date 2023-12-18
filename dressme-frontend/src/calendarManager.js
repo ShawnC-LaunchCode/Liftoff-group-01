89922bd28a0eb30b41777fe22af182ba5bde9a43
Colin Robinson <clnjrobinson@gmail.com>
2023-12-18 11:01:40 -0600

    Finished basic outline of the calendar, still need to add funtionality to add events.

--- dressme-frontend/src/calendarManager.js
+++ dressme-frontend/src/calendarManager.js
@@ -1,8 +1,9 @@
-let date = new Date();
-let day = date.getDate();
-let year = date.getFullYear();
-let month = date.getMonth();
-let calendar = document.querySelector("calendar");
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth();
+const year = date.getFullYear();
+const calendar = document.getElementById('calendar');
+const currentDate = document.querySelector(".calendar-current-date");
 
 const months = [
     "January",
@@ -30,7 +31,36 @@ const weekdays = [
 ];
 
 function load() {
-    const firstDayInMonth = new Date(year, month, 1);
-    const daysInMonth = new Date(year, month + 1, 0).getDate();
-    console.log(daysInMonth);
+    const firstDay = new Date(year, month, 1);
+    const lastDate = new Date(year, month + 1, 0).getDate();
+    const lastDay = new Date(year, month, lastDate);
+
+    console.log(lastDay);
+
+    const dateString = firstDay.toLocaleDateString('en-us', {
+        weekday: "long",
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+    })
+
+    currentDate.innerText = `${months[month]} ${year}`;
+
+    const paddingDay = weekdays.indexOf(dateString.split(", ")[0]);
+
+    for (let i = 1; i < paddingDay + lastDate; i++) {
+        const calendarSquare = document.createElement('div');
+        calendarSquare.classList.add('day');
+
+        if (i > paddingDay) {
+            calendarSquare.innerText = i - paddingDay;
+
+            calendarSquare.addEventListener("click", () => console.log("click"));
+        } else {
+            calendarSquare.classList.add('padding')
+        }
+        calendar.appendChild(calendarSquare);
+    }
 }
+
+load();
