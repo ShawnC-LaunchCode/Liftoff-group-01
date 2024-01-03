d408400239f65a494a19304084682b47c3dc0def
Colin Robinson <clnjrobinson@gmail.com>
2024-01-03 15:18:09 -0600

    Calendar adds and deletes events

--- dressme-frontend/src/calendarManager.js
+++ dressme-frontend/src/calendarManager.js
@@ -3,7 +3,10 @@ let clicked = null;
 let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
 
 const calendar = document.getElementById('calendar');
-
+const newEventModal = document.getElementById('newEventModal');
+const deleteEventModal = document.getElementById('deleteEventModal');
+const backDrop = document.getElementById('modalBackDrop');
+const eventTitleInput = document.getElementById('eventTitleInput');
 const weekdays = [
     "Sunday", 
     "Monday",
@@ -13,6 +16,21 @@ const weekdays = [
     "Friday",
     "Saturday"
 ];
+
+function openModal(date) {
+    clicked = date;
+
+    const eventForDay = events.find(e => e.date == clicked);
+
+    if (eventForDay) {
+        document.getElementById('eventText').innerText = eventForDay.title;
+        deleteEventModal.style.display = 'block';
+    } else {
+        newEventModal.style.display = 'block';
+    }
+
+    backDrop.style.display = 'block';
+}
 
 function load() {
     const date = new Date();
@@ -44,16 +62,62 @@ function load() {
         const daySquare = document.createElement('div');
         daySquare.classList.add('day');
 
+        const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+
         if (i > paddingDays) {
             daySquare.innerText = i - paddingDays;
+            const eventForDay = events.find(e => e.date == dayString);
 
-            daySquare.addEventListener('click', () => console.log('click'));
+            if (i - paddingDays == day && nav == 0) {
+                daySquare.id = 'currentDay';
+            }
+
+            if (eventForDay) {
+                const eventDiv = document.createElement('div');
+                eventDiv.classList.add('event');
+                eventDiv.innerText = eventForDay.title;
+                daySquare.appendChild(eventDiv);
+            }
+
+            daySquare.addEventListener('click', () => openModal(dayString));
         } else {
             daySquare.classList.add('padding');
         }
 
         calendar.appendChild(daySquare);
     }
+}
+
+function closeModal() {
+    eventTitleInput.classList.remove('error');
+    newEventModal.style.display = 'none';
+    deleteEventModal.style.display = 'none';
+    backDrop.style.display = 'none';
+    eventTitleInput.value = '';
+    clicked = null;
+    load();
+}
+
+function saveEvent() {
+    if (eventTitleInput.value) {
+        eventTitleInput.classList.remove('error');
+
+        events.push({
+            date: clicked,
+            title: eventTitleInput.value,
+        });
+
+        localStorage.setItem('events', JSON.stringify(events));
+        closeModal();
+    } else {
+        eventTitleInput.classList.add('error');
+    }
+}
+
+function deleteEvent() {
+    events = events.filter(e => e.date !== clicked);
+    localStorage.setItem('events', JSON.stringify(events));
+    closeModal();
 }
 
 function initButtons() {
@@ -66,6 +130,12 @@ function initButtons() {
         nav--;
         load();
     });
+
+    document.getElementById('saveButton').addEventListener('click', saveEvent);
+    document.getElementById('cancelButton').addEventListener('click', closeModal);
+
+    document.getElementById('deleteButton').addEventListener('click', deleteEvent);
+    document.getElementById('closeButton').addEventListener('click', closeModal);
 }
 
 initButtons();
