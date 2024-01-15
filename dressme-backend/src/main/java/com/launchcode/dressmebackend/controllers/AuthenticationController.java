40a6a2a6ca9a0baf2cca584613ab6643c16093ee
Brittany Robinson <bncopple@gmail.com>
2024-01-15 17:16:20 -0600

    starting over

--- dressme-backend/src/main/java/com/launchcode/dressmebackend/controllers/AuthenticationController.java
+++ dressme-backend/src/main/java/com/launchcode/dressmebackend/controllers/AuthenticationController.java
@@ -10,15 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import com.launchcode.dressmebackend.models.dto.LoginFormDTO;
 import com.launchcode.dressmebackend.models.dto.RegisterFormDTO;
 
 import java.util.Optional;
 
 @Controller
+@RestController
+@CrossOrigin (origins = "*")
+
 public class AuthenticationController {
 
     @Autowired
--- dressme-frontend/src/Pages/Register.js
+++ dressme-frontend/src/Pages/Register.js
@@ -1,22 +1,65 @@
 import React from "react";
+import useState from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 function Register() {
+    const[firstName,setFirstName]=useState("");
+    const[lastName, setLastName]= useState("");
+  
+    const[email, setEmail]=useState(false);
+    const navigate=useNavigate();
+    const [password, setPassword] = useState([]);
+    
+
+
+    const handlesubmit=(e)=>{
+      e.preventDefault();
+      const data={name,email,password};
+      
+
+      fetch("http://localhost:8080/register",{
+        method:"POST",
+        headers:{"content-type":"application/json"},
+        body:JSON.stringify(data)
+      }).then((res)=>{
+        //alert('Saved successfully.')
+        Swal.fire({icon: "success",
+        title:"Registration Successful!"});
+        navigate('/Login');
+      }).catch((err)=>{
+        console.log(err.message)
+      })
+    //   const newUser = {
+    //     name: data.name,
+    //     email: data.email,
+    //     password: data.password
+    //   }
+    //   const newContacts = [...records, newContact];
+    //   setRecords(newContacts);
+
+    // }
+
+
+
+
    
     return (
       <div className="CreateAccount">
         <h1 class="CreateAccountPageHeader">Welcome to Weather2Wear!</h1>
   <h3>Create a New Account Below</h3>
   <div>
-  <form action="/api/Register" method="post">
-     <label>First Name <input type="text" name="firstName"/> </label>
-      <label>Last Name <input type="text" name="lastName"/> </label>
-      <label>Email <input type="email" name="emailAddress"/></label>
-      <label>Verify Password <input type="email" name="verifyEmailAddress"/></label>
-      <input type="submit" formAction="./App.js" value="createAccount"/>
+  <form onSubmit={handlesubmit}>
+     <label>First Name <input type="text" value={firstName} /> </label>
+      <label>Last Name <input type="text" value={lastName}/> </label>
+      <label>Email <input type="email" value={email}/></label>
+      <label>Password<input type="text" value={password}/></label>
+      <br />
+      <button type="submit">Register</button>
   </form>
   </div>
       </div>
     );
   }
-  
+}
   export default Register;
