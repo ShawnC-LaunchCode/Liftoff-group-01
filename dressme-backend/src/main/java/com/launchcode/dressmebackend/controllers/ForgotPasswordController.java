7eded16004f3c31b5dc6a949838fb05758bd9338
Brittany Robinson <bncopple@gmail.com>
2024-01-16 12:57:11 -0600

    springboot mail dependency added and controller updated

--- dressme-backend/src/main/java/com/launchcode/dressmebackend/controllers/ForgotPasswordController.java
+++ dressme-backend/src/main/java/com/launchcode/dressmebackend/controllers/ForgotPasswordController.java
@@ -1,28 +1,46 @@
 package com.launchcode.dressmebackend.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 public class ForgotPasswordController {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    private Map<String,String> resetTokens= new HashMap<>();
     @PostMapping("/forgot-password")
     public ResponseEntity<?> forgotPassword(@RequestBody Map<String, String> request) {
         String email = request.get("email");
 
         // TODO: Implement logic to generate a password reset token and send an email
+        String resetToken = UUID.randomUUID().toString();
 
-        sendResetEmail(email);
+        resetTokens.put(email, resetToken);
 
+        sendResetEmail(email, resetToken);
 
         return ResponseEntity.ok(Map.of("success", true, "message", "Password reset email sent."));
+
     }
 
-    private void sendResetEmail(String email) {
-        // TODO: Implement sending a password reset email
 
+    private void sendResetEmail(String email, String resetToken) {
+        SimpleMailMessage message= new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("Password Reset Request");
+        message.setText("To reset your password, click the following link: "+"http://localhost:8080/reset-password?token="+ resetToken);
+
+        javaMailSender.send(message);
     }
 }
