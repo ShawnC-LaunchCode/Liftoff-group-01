912d9df2cf314641b3032d929e52669b73310e80
Brittany Robinson <bncopple@gmail.com>
2024-01-15 19:33:31 -0600

    set zip code form front end made

---
+++ dressme-backend/src/main/java/com/launchcode/dressmebackend/data/SettingsRepository.java
@@ -0,0 +1,4 @@
+package com.launchcode.dressmebackend.data;
+
+public class SettingsRepository {
+}
--- dressme-frontend/src/components/SetZipCode.js
+++ dressme-frontend/src/components/SetZipCode.js
@@ -1,15 +1,50 @@
-import React from "react";
+import React, { useState} from "react";
 
-export default function SetZipCode() {
-    return (
-      <div className="SetZipCode">
-      <h3> Set Your Local 6-Digit Zip Code Below </h3>
-      <form>
+export default function SetZipCode(userId) {
 
-      </form>
+  const [newZipcode, setNewZipcode] = useState('');
+
+  const handleZipcodeChange = (e) => {
+    setNewZipcode(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = { userId, newZipcode };
+    console.log(data);
+
+    fetch("http://localhost:8080/Zipcode", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        // Add any additional logic based on the response from the backend
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Update Zipcode</h2>
+      <label>
+        New Zipcode:
+        <input
+          type="text"
+          value={newZipcode}
+          onChange={handleZipcodeChange}
+        />
+      </label>
+      <button type="submit">Update Zipcode</button>
+    </form>
+  );
+};
 
 
-      </div>
-    );
-  }
-  
+
+    
