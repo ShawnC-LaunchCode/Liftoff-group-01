26a30c0af904c1242c1131d965b17524b1cd6685
Colin Robinson <clnjrobinson@gmail.com>
2024-01-18 11:43:18 -0600

    Worked more on calendar appearnce in the React app.

--- dressme-backend/src/main/java/com/launchcode/dressmebackend/models/Event.java
+++ dressme-backend/src/main/java/com/launchcode/dressmebackend/models/Event.java
@@ -1,17 +1,17 @@
 package com.launchcode.dressmebackend.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-@Entity
-public class Event {
-
-    @Id
-    @GeneratedValue
-    private int id;
-    @NotNull
-    private String event;
-}
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.Id;
+//import jakarta.validation.constraints.NotBlank;
+//import jakarta.validation.constraints.NotNull;
+//
+//@Entity
+//public class Event {
+//
+//    @Id
+//    @GeneratedValue
+//    private int id;
+//    @NotNull
+//    private String event;
+//}
